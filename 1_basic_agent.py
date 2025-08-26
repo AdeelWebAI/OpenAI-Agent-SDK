09cb76b5a7629fcc5994ad6067ccacb9c21cd8f0
adeel <adeelwaraich27@gmail.com>
2025-08-26 23:26:32 +0500

    opeai AgentSDK basic model

---
+++ 1_basic_agent.py
@@ -0,0 +1,27 @@
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel,Runner
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are an expert of Agentic AI.",
+    model= OpenAIChatCompletionsModel(model="gemini-2.0-flash",openai_client=client)
+)
+
+query = input("Enter your query")
+
+result = Runner.run_sync(
+    agent,
+    query,
+)
+print(result.final_output)
